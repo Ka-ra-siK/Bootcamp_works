d4e41743a68ac798d9ab31b9215fa72cc4ba967f
Konstantin Konovalov <53082951+Ka-ra-siK@users.noreply.github.com>
2022-07-14 15:22:23 +0700

    Update zoom.js

--- OnlineStore/js/zoom.js
+++ OnlineStore/js/zoom.js
@@ -1,4 +1,8 @@
-function zoomImage(id, img) {
-    var inp = document.getElementById(id);
-    inp.src = img;
-}
+const images = document.querySelectorAll('.product__image');
+images.forEach(item => {
+  item.addEventListener('mouseenter', event => {
+    document.querySelectorAll('.product__image--main').forEach(mainPhoto => {
+      mainPhoto.setAttribute('src', item.getAttribute('src'));
+    })
+  });
+});
